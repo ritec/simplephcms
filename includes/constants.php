239a502c76f882926f829b8448df72eb72b75ecc
Ri Caragol <rcaragol@iu.edu>
2012-12-25 18:17:09 -0500

    changed constants

--- includes/constants.php
+++ includes/constants.php
@@ -2,8 +2,8 @@
 
 // Database Constants
 define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "OtlPHP07");
+define("DB_USER", "someuser");
+define("DB_PASS", "somepassword");
 define("DB_NAME", "widget_corp");
 
 ?>
